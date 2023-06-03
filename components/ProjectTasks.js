df6918218e7aeb2f1d809707c60b0b44f2f85b54
unknown <allamedd37@gmail.com>
2023-06-03 16:17:10 +0100

    add new task feature

--- components/ProjectTasks.js
+++ components/ProjectTasks.js
@@ -1,8 +1,11 @@
-import {View, Text, ScrollView} from "react-native";
+import {View, Text, ScrollView, Pressable, Modal, TextInput, StyleSheet} from "react-native";
 import UserAvatar from "./ui/UserAvatar";
-import React from "react";
+import React, {useState} from "react";
 import {Avatar} from "@rneui/themed";
 import {stringToColor} from "../utils/utilsFunctions";
+import CurrentTask from "../screens/CurrentTask";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {Button} from "@rneui/base";
 const taskStatus=(status)=>{
     //En_ATTENTE, EN_COURS, ATTENTE_VALIDATION,VALIDEE
     switch (status) {
@@ -39,47 +42,147 @@ const taskStatus=(status)=>{
 
     }
 }
+
 const ProjectTasks = ({tasks}) => {
-    return(
-        <ScrollView style={{margin:20}}>
-            <Text>Tasks :</Text>
-            {tasks.map((task)=>(
-                <View style={{}} key={task.id}>
-                    <View style={{flexDirection:"column", alignItems:"center", margin:5}}>
-                        <View style={{flexDirection:"row", alignItems:"center"}}>
+    const [currentTask, setCurrentTask] = useState('initial value');
+    const Stack = createNativeStackNavigator();
+    const AllProjectTasks = ({tasks}) => {
+        const [newTaskModalVisible, setNewTaskModalVisible] = useState(false);
+        return(
+            <ScrollView style={{marginVertical:5}}>
 
-                        <View style={{flex:1}}>
-                            <Avatar
-                                rounded
-                                title={task.titre.charAt(0).toUpperCase()}
-                                size={40}
-                                containerStyle={{ backgroundColor: stringToColor(task.titre) }}
-                            />
+                {tasks.map((task)=>(
+                    <Pressable style={{ borderBottomColor: "grey", borderBottomWidth:0.5, padding:5}} key={task.id}
+                               onPress={
+                                   ()=>{
+                                       console.log("task pressed");
+                                   }
+                               }
+                    >
+                        <View style={{flexDirection:"column", alignItems:"center", marginVertical:10, marginHorizontal:10}}>
+                            <View style={{flexDirection:"row", alignItems:"center"}}>
+
+                                <View style={{flex:1, }}>
+                                    <Avatar
+                                        rounded
+                                        title={task.titre.charAt(0).toUpperCase()}
+                                        size={40}
+                                        containerStyle={{ backgroundColor: stringToColor(task.titre) }}
+                                    />
+                                </View>
+                                <View style={{flex:5, flexDirection:"column", marginLeft:15}}>
+                                    <Text style={{marginLfet:5}}>{task.titre}</Text>
+                                    <Text style={{color:"grey", fontSize:12}}> {"assigned to "+ task.responsable.nom + " on " + task.deadLine }</Text>
+
+                                </View>
+
+                                <View style={{marginRight:10}}>
+                                    <Text style={{
+                                        color:taskStatus(task.status).color,
+                                        fontSize:12,
+                                    }}>
+                                        {taskStatus(task.status).state}
+                                    </Text>
+                                </View>
+                            </View>
+
+
                         </View>
-                        <View style={{flex:5, flexDirection:"column"}}>
-                            <Text>{task.titre}</Text>
-                            <Text style={{color:"grey", fontSize:12}}> {"assigned to "+ task.responsable.nom + " on " + task.deadLine }</Text>
 
-                        </View>
-
-                        <View style={{marginRight:10}}>
-                            <Text style={{
-                                color:taskStatus(task.status).color,
-                                fontSize:12,
-                            }}>
-                            {taskStatus(task.status).state}
-                            </Text>
-                        </View>
-                        </View>
-
-
-                    </View>
+                    </Pressable>
+                ))}
+                <View style={{margin:25}}>
+                    <Button title={"add new task"} onPress={()=>
+                    setNewTaskModalVisible(true)
+                    }/>
 
                 </View>
-            ))}
+                <Modal
+                    style={styles.modalContainer}
+                visible={newTaskModalVisible}
+                animationType="slide"
+                transparent={true}
+                >
+                    <View style={styles.modalContent}>
+                        <View style={{flexDirection:"row", alignItems:"center", margin:5}}>
+                            <Text style={{flex:1, margin:5}}>Task title</Text>
+                            <TextInput style={{flex:5, margin:5}}/>
 
-        </ScrollView>
 
-    );
+                        </View>
+                    </View>
+                </Modal>
+
+
+            </ScrollView>
+
+        );
+    }
+
+    const ProjectCurrentTask = ({currentTask}) => {
+        return(
+            <View>
+                <Text > project task details </Text>
+            </View>
+        );
+    }
+return(
+    <Stack.Navigator
+  headerShown={false}
+    >
+        <Stack.Screen name="AllProjectTasks"
+         headerShown={false}
+        >
+            {props => <AllProjectTasks {...props} setCurrentTask={setCurrentTask}  tasks={tasks}/>}
+        </Stack.Screen>
+        <Stack.Screen name="Project Task details"  headerShown={false}>
+            {props => <ProjectCurrentTask {...props} currentTask={currentTask} />}
+        </Stack.Screen>
+    </Stack.Navigator>);
+
+
+
 }
 export default ProjectTasks;
+const styles = StyleSheet.create( {
+    modalContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#00000080',
+    },
+    modalHeader: {
+        width: '100%',
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#000000',
+        paddingVertical: 10,
+
+
+
+    },
+    modalContent: {
+        width: '80%',
+        height: '50%',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalText: {
+        fontSize: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        textAlign: 'center',
+    },
+    modalClose: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#0000FF',
+        marginTop: 10,
+
+    },
+}
+);
